e693b5dcefcc1ee32bdfcd53f9689c977e52d285
this-is-my-username-guys <163167348+this-is-my-username-guys@users.noreply.github.com>
2026-09-24 20:53:34 -0600

    Completed Prep 3 and Stretch requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play="yes";
+        do{
+            Console.WriteLine("What is the magic number? ");
+            int num= int.Parse(Console.ReadLine());
+            int guess;
+            int tries=0;
+            do
+            {
+                Console.WriteLine("What is your guess? ");
+                guess= int.Parse(Console.ReadLine());
+                if (guess==num)
+                {
+                    tries++;
+                    Console.WriteLine("You Guessed it");
+                    Console.WriteLine($"You took {tries} guesses");
+                }
+                
+                else if (guess>num)
+                {
+                    tries++;
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guess<num)
+                {
+                    tries++;
+                    Console.WriteLine("Higher");
+                }
+            }while (guess!=num);
+
+            Console.WriteLine("Would you like to play again? ");
+            play=Console.ReadLine().ToLower();
+        }while (play=="yes");
     }
 }
