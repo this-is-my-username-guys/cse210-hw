e64a258575376dc2b2b46920eb15d02406779751
this-is-my-username-guys <163167348+this-is-my-username-guys@users.noreply.github.com>
2026-09-25 17:11:25 -0600

    Changed Console.WriteLine() to Console.Write() for Output formatting purposes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
-        Console.WriteLine("What is your last name?");
+        Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
