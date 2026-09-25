9335466841d2a97eeb1046344b4a7a2087eed80a
this-is-my-username-guys <163167348+this-is-my-username-guys@users.noreply.github.com>
2026-09-25 17:13:24 -0600

    Completed Prep4 and Prep5 With the stretch activity and additional code enhancements

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //using float in case program encounters a decimal. I like to improve my programs :)
+        //I know it takes more space but this is a practice assignment so I did this for fun
+        float temp = 0;
+        float sum = 0;
+        float average;
+        float large;
+        float? small = null;
+        List<float> num = new List<float>();
+        do
+        {
+            Console.Write("Enter Number: ");
+            temp=float.Parse(Console.ReadLine());
+            if(temp!=0)
+            {
+                num.Add(temp);
+            }
+        }while (temp!=0);
+        
+        num.Sort();
+        
+        foreach (float i in num)
+        {
+            sum+=i;
+        }
+
+        average = sum/num.Count;
+
+        large=num[num.Count-1];
+
+        foreach (float i in num)
+        {
+            if (i>0)
+            {
+                small=i;
+                break;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}"); 
+        Console.WriteLine($"The largest number is: {large}");
+
+        if (small!=null)
+        {
+            Console.WriteLine($"The smallest positive number is: {small}");
+        }
+        else
+        {
+            Console.WriteLine("No positive number was entered.");
+        }
+
+        Console.WriteLine("The sorted list is: ");
+        foreach (float i in num)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favourite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter your birth year: ");
+        year=int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num*num;
+    }
+
+    static void DisplayResult(string a, int b, int c)
+    {
+        Console.WriteLine($"{a}, the square of your number is {b}");
+        Console.WriteLine($"{a}, you will turn {c} this year");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string username = PromptUserName();
+        int usernumber = PromptUserNumber();
+        int birthyear; PromptUserBirthYear(out birthyear);
+        int age = DateTime.Now.Year - birthyear;
+        int square = SquareNumber(usernumber);
+
+        DisplayResult(username,square,age);
+
     }
 }
